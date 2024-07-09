cb2bfbb4083b33d9cac766e31dbabcfef76f792e
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-07-09 20:26:10 +0300

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""  a coroutine function """
+
+import asyncio
+import random
+from typing import List
+
+
+async def async_generator() -> List[float]:
+    """ yields a random number between 0 and 10 """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
